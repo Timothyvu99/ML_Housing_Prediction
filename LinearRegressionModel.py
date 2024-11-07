8cad047480668d6f9144669945ed2d8b0a20f581
Timothyvu99 <timothyvu99@gmail.com>
2024-11-07 00:54:18 -0800

    Extra Linear Regression Details

--- LinearRegressionModel.py
+++ LinearRegressionModel.py
@@ -4,6 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import statsmodels.api as sm
 
 #Load the dataset
 housing_all = pd.read_csv('Housing.csv')
@@ -15,19 +18,51 @@ housing = housing_all[['price', 'area', 'bedrooms', 'bathrooms', 'stories', 'mai
 x = housing[['area', 'bedrooms']].values.reshape(-1, 2)
 y = housing[['price']].values.reshape(-1, 1)
 
+#Split data
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.5, random_state = 42)
+
 #initialize multiple linear regression model
 multRegMod = LinearRegression()
 
 #Fit multiple linear regression model
-multRegMod.fit(x, y)
+multRegMod.fit(x_train, y_train)
 
 #Print intercept and weight
 print(f"Multiple Regression Model Intercept: {multRegMod.intercept_}")
 print(f"Multiple Regression Model Weight: {multRegMod.coef_}")
 
-yPredicted = multRegMod.predict(x)
+#Predicted and Residual
+yPredicted = multRegMod.predict(x_train)
+residual = y_train - yPredicted
 
 print(f"Predicted Price: {yPredicted[:6]}")
+print(f"Residual: {residual[:6]}")
+
+#Accuracy
+accuracy = multRegMod.score(x, y)
+
+print("Accuracy: ", end="")
+print("%.2f" % (100*accuracy))
+
+#Adjusted R-squared
+n = x_train.shape[0]
+p = x_train.shape[1]
+adjusted_accuracy = 1 - (1-accuracy) * (n - 1) / (n - p - 1)
+print("Adjusted Accuracy: ", end="")
+print("%.2f" % (100*adjusted_accuracy))
+
+#Mean Squared Error
+mse = mean_squared_error(y_train, yPredicted)
+print(f"Mean Squared Error: {mse}")
+
+#Mean Absolute Error
+mae = mean_absolute_error(y_train, yPredicted)
+print(f"Mean Absolute Error: {mae}")
+
+x_train_const = sm.add_constant(x_train)
+sm_model = sm.OLS(y_train, x_train_const).fit()
+print(f"P-values: {sm_model.pvalues}")
+print(f"Confidence Intervals: {sm_model.conf_int()}")
 
 # Create grid for prediction surface
 Xvals=np.linspace(min(housing['area']), max(housing['area']),20)
